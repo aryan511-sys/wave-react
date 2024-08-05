bf071c0e04856347fe7c3fb3d0c728b2c72113cb
aryan511-sys <gajerarajesh4317@gmail.com>
2024-08-05 18:29:58 +0530

    Add some changes

---
+++ src/Components/Gallery.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+
+const Gallery = () => {
+  return (
+    <div>
+      <section className="tw-text-gray-600 tw-body-font mt-3 ">
+      <div className="container mb-5 mb-md-6 mb-xl-10">
+        <div className="row justify-content-md-center">
+          <div className="col-12 col-md-10 col-lg-9 col-xl-8 col-xxl-7 text-center container">
+            <h2 className="display-3 fw-bolder mb-4 tw-text-5xl sm:tw-text-1xl clas-res">Meet our portfolio to<br/><span className="linear-g">kickstart</span>your success.</h2>
+          </div>
+        </div>
+      </div>
+
+      <div className="tw-flex tw-justify-between container max-md:tw-block max-md:tw-pl-10">
+        <div className="tw-m-auto">
+         
+            <div className="width tw-rounded-2xl tw-relative tw-overflow-hidden disable mb-3">
+              <img alt="gallery" className="tw-w-full image-001"
+                src="https://bootstrapbrain.com/demo/templates/wave/wave-lite/assets/img/portfolio/project-landscape-1.jpg"/>
+              <div className="tw-w-full tw-h-full tw-bg-transparent tw-absolute tw-top-0 tw-right-0 tw-flex tw-items-center tw-justify-center tw-flex-nowrap tw-flex-col right-001 sub" >
+                <h2 className="tw-text-2xl ">Canvas lover</h2>
+                <p className="">Photography</p>
+              </div>
+            </div>
+          
+
+            <div className="width tw-rounded-2xl tw-relative tw-overflow-hidden disable mb-3">
+              <img alt="gallery" className="tw-w-full image-001"
+                src="https://bootstrapbrain.com/demo/templates/wave/wave-lite/assets/img/portfolio/project-portrait-2.jpg"/>
+              <div className="tw-w-full tw-h-full tw-bg-transparent tw-absolute tw-top-0 tw-right-0 tw-flex tw-items-center tw-justify-center tw-flex-nowrap tw-flex-col right-001 sub" >
+                <h2 className="tw-text-2xl ">Wavy Road</h2>
+                <p className="">Advanture</p>
+              </div>
+          </div>
+            <div className="width tw-rounded-2xl tw-relative tw-overflow-hidden disable mb-3">
+              <img alt="gallery" className="tw-w-full image-001"
+                src="https://bootstrapbrain.com/demo/templates/wave/wave-lite/assets/img/portfolio/project-landscape-3.jpg"/>
+                <div className="tw-w-full tw-h-full tw-bg-transparent tw-absolute tw-top-0 tw-right-0 tw-flex tw-items-center tw-justify-center tw-flex-nowrap tw-flex-col right-001 sub" >
+                  <h2 className="tw-text-2xl ">Bright-Winks</h2>
+                  <p className="">Design</p>
+                </div>
+          </div>
+        </div>
+
+        <div className="">
+            <div className="width tw-rounded-2xl tw-relative tw-overflow-hidden disable mb-3">
+              <img alt="gallery" className="tw-w-full image-001"
+                src="https://bootstrapbrain.com/demo/templates/wave/wave-lite/assets/img/portfolio/project-portrait-1.jpg"/>
+                <div className="tw-w-full tw-h-full tw-bg-transparent tw-absolute tw-top-0 tw-right-0 tw-flex tw-items-center tw-justify-center tw-flex-nowrap tw-flex-col right-001 sub" >
+                  <h2 className="tw-text-2xl ">Red-Lava</h2>
+                  <p className="">Nature</p>
+                </div>
+          </div>
+          <div className="width tw-rounded-2xl tw-relative tw-overflow-hidden disable mb-3">
+              <img alt="gallery" className="tw-w-full image-001"
+                src="https://bootstrapbrain.com/demo/templates/wave/wave-lite/assets/img/portfolio/project-portrait-4.jpg"/>
+                <div className="tw-w-full tw-h-full tw-bg-transparent tw-absolute tw-top-0 tw-right-0 tw-flex tw-items-center tw-justify-center tw-flex-nowrap tw-flex-col right-001 sub" >
+                  <h2 className="tw-text-2xl ">Minimal Notion</h2>
+                  <p className="">Design</p>
+                </div>
+          </div>
+        </div>
+
+        <div>
+            <div className="width tw-rounded-2xl tw-relative tw-overflow-hidden disable mb-3">
+              <img alt="gallery" className="tw-w-full image-001"
+                src="https://bootstrapbrain.com/demo/templates/wave/wave-lite/assets/img/portfolio/project-landscape-2.jpg"/>
+                <div className="tw-w-full tw-h-full tw-bg-transparent tw-absolute tw-top-0 tw-right-0 tw-flex tw-items-center tw-justify-center tw-flex-nowrap tw-flex-col right-001 sub" >
+                  <h2 className="tw-text-2xl ">Jungle Book</h2>
+                  <p className="">Design</p>
+                </div>
+          </div>
+          <div className="width tw-rounded-2xl tw-relative tw-overflow-hidden disable mb-3">
+              <img alt="gallery" className="tw-w-full image-001"
+                src="https://bootstrapbrain.com/demo/templates/wave/wave-lite/assets/img/portfolio/project-portrait-3.jpg"/>
+                <div className="tw-w-full tw-h-full tw-bg-transparent tw-absolute tw-top-0 tw-right-0 tw-flex tw-items-center tw-justify-center tw-flex-nowrap tw-flex-col right-001 sub" >
+                  <h2 className="tw-text-2xl ">Golder Leaves</h2>
+                  <p className="">Photography</p>
+                </div>
+          </div>
+          <div className="width tw-rounded-2xl tw-relative tw-overflow-hidden disable mb-3">
+              <img alt="gallery" className="tw-w-full image-001"
+                src="https://bootstrapbrain.com/demo/templates/wave/wave-lite/assets/img/portfolio/project-landscape-4.jpg"/>
+                <div className="tw-w-full tw-h-full tw-bg-transparent tw-absolute tw-top-0 tw-right-0 tw-flex tw-items-center tw-justify-center tw-flex-nowrap tw-flex-col right-001 sub" >
+                  <h2 className="tw-text-2xl ">Innovative Day</h2>
+                  <p className="">Photography</p>
+                </div>
+          </div>
+        </div>
+
+      </div>
+      </section>
+      </div>
+
+  )
+}
+
+export default Gallery
